01eeb4191f946aa374c2173f4719f0c2fd885b97
Erica Altamirano <erica.altamirano@tictapps.com>
2019-04-26 20:16:42 -0300

    Eliminado lo referente a seguridad

--- src/main/java/com/tip/futbolifybo/api/config/JerseyConfig.java
+++ src/main/java/com/tip/futbolifybo/api/config/JerseyConfig.java
@@ -3,11 +3,6 @@ package com.tip.futbolifybo.api.config;
 import com.tip.futbolifybo.api.AuthorizeREST;
 import com.tip.futbolifybo.api.CORSResponseFilter;
 import com.tip.futbolifybo.api.SpaceREST;
-import com.tip.futbolifybo.api.provider.ObjectMapperProvider;
-import com.tip.futbolifybo.security.api.exceptionmapper.AccessDeniedExceptionMapper;
-import com.tip.futbolifybo.security.api.exceptionmapper.AuthenticationExceptionMapper;
-import com.tip.futbolifybo.security.api.exceptionmapper.AuthenticationTokenRefreshmentExceptionMapper;
-import com.tip.futbolifybo.security.api.resource.AuthenticationResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
@@ -24,12 +19,6 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(CORSResponseFilter.class);
-        register(AuthenticationResource.class);
-
-        register(AccessDeniedExceptionMapper.class);
-        register(AuthenticationExceptionMapper.class);
-        register(AuthenticationTokenRefreshmentExceptionMapper.class);
-        register(ObjectMapperProvider.class);
 
         register(SpaceREST.class);
         register(AuthorizeREST.class);
